5aa4481f95f3de1a690854818d8b7b46653e4422
= <mpad0004@student.monash.edu>
2024-07-26 22:31:17 +1000

    Updated TaxCalculator to reflect all income brackets, updated interface to demonstrate MVP

--- src/TaxCalculator.java
+++ src/TaxCalculator.java
@@ -13,7 +13,12 @@ public class TaxCalculator {
     public TaxCalculator(double income){
         this.income = income;
         calculateTaxLiability();
-        showTaxDetails();
+        //showTaxDetails();
+    }
+
+    // Getter: Return tax liability
+    public double getTaxLiability(){
+        return this.taxLiability;
     }
 
     // Based on income, calculate tax payable
@@ -21,15 +26,26 @@ public class TaxCalculator {
         // Calculate tax payable for each tax bracket
         // 0-$18,2000 : Nil
 
-        // $18,201 - $45,000: 16c for each $1 over $18,200
-        if (this.income > 190001){
-            this.taxLiability =  (51638 + .45 * (this.income - 190000));
-        }else if (this.income >= 135001 && this.income <= 190000){
-            this.taxLiability += (31288 + .37 * (this.income - 135000));
+        // >= $190,001: $51,638 + 45c for each dollar over $190,000
+        if (this.income >= 190001){
+            this.taxLiability = (51638 + .45 * (this.income - 190000));
+
+        // $135,001 - $190,000: $31,288 + 37c for each dollar over $135,000
+        }else if (this.income >= 135001){
+            this.taxLiability = (31288 + .37 * (this.income - 135000));
+
+        // $45,001 - $135,000: $4,288 + 30c for each dollar over $45,000
+        }else if (this.income >= 45001){
+            this.taxLiability = (4288 + .30 *(this.income - 45000));
+
+        // $18,201 - $45,000: 16c for each dollar over $18,200
+        }else if (this.income >= 18201){
+            this.taxLiability = (.16 * (this.income - 18201));
         }else{
-            // TODO: Add other tax brackets
             this.taxLiability = 0;
         }
+        // Round number to 2 decimal places
+        this.taxLiability = Math.round(this.taxLiability * 100.0) / 100.0;
     }
 
 
